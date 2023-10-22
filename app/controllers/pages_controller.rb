cb76f95025c8c40f797ecf60690aea05279ec745
Eduardo <ducu_yusuke@hotmail.com>
2023-10-22 14:34:23 -0300

    small changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :teste ]
 
   def home
-    @graph_data = [12, 19, 3, 5, 2, 3].to_json
+    json_data = File.read('dataproc/teste.json')
+    @graph_data = JSON.parse(json_data)
+    # @graph_data = [12, 19, 3, 5, 2, 3].to_json
     @graph = [12, 19, 3, 5, 2, 3].to_json
     @graph_polar = [20, 30, 55, 10, 35, 40].to_json
-  end
-
-  def teste
   end
 end
