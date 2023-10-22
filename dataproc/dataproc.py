2e2108b74a8b3d03882c41a2368917150942ae20
Bruno Velasquez <velasquez.bruno@gmail.com>
2023-10-22 13:23:59 -0300

    dataproc update

--- dataproc/dataproc.py
+++ dataproc/dataproc.py
@@ -1,12 +1,31 @@
 import pandas as pd
+import sys
 
-df = pd.read_csv('../raw_data/primeira_extracao.csv')
+def data_proc(data_inicio, data_fim):
+    '''
+    data_inicio = str
+    data_fim = str
+    '''
+    df = pd.read_csv('../raw_data/primeira_extracao.csv')
 
-df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
 
-teste = df.groupby(['date','Campaign Name'])\
-.agg({'Reach (Estimated)':'sum'})\
-.reset_index()\
-.pivot_table(index='date', columns='Campaign Name')
+    # Definindo as datas de início e fim para o período desejado
+    data_inicio = pd.to_datetime(data_inicio)
+    data_fim = pd.to_datetime(data_fim)
 
-teste.to_json('teste.json')
+    # Filtrando o DataFrame entre as datas de início e fim
+    df_filtrado = df.loc[(df['Date'] >= data_inicio) & (df['Date'] <= data_fim)]
+
+    teste = df_filtrado.groupby(['Date','Campaign Name'])\
+    .agg({'Reach (Estimated)':'sum'})\
+    .reset_index()\
+    .pivot_table(index='Date', columns='Campaign Name')
+
+    teste.to_json('teste.json')
+
+if __name__ == "__main__":
+    data_inicio = sys.argv[1]
+    data_fim = sys.argv[2]
+
+    data_proc(data_inicio, data_fim)
