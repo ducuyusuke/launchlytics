cfdfcb26e14099ddc4f85aa775196b8e2036f50c
victorgqueiroz <victorg.queiroz@hotmail.com>
2023-10-22 13:36:33 -0300

    dashboard overview

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @graph_data = [12, 19, 3, 5, 2, 3].to_json
+    @graph = [12, 19, 3, 5, 2, 3].to_json
+    @graph_polar = [20, 30, 55, 10, 35, 40].to_json
   end
 
   def teste
